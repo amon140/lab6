3ee58a6937f29bc2ca8c1e316a50ee1d24d583f3
Amon Neequaye <aneequay@ucsd.edu>
2015-02-13 13:39:30 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,15 +9,17 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+	$('.project a').click(addProjectDetails); 		
 
 	$('#colorBtn').click(randomizeColors);
 }
 
+var details;
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+
 	// Prevent following the link
 	e.preventDefault();
 
@@ -26,8 +28,24 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	details = $(this).next();
+
+	var url = "/project/" + idNumber;
+	$.get(url, project);
+
 	console.log("User clicked on project " + idNumber);
+	console.log(url);
 }
+
+function project(result){
+	var projectHTML = '<a href="#" class="thumbnail">' + 
+		'<img src="' + result['image'] + '" class="img">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>';
+
+	details.html(projectHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +53,9 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette/", colorFun);
+}
+
+function colorFun() {
+	console.log("Hola");
 }
--- routes/palette.js
+++ routes/palette.js
@@ -2,6 +2,8 @@ var palettes = require('../palettes.json');
 
 exports.randomPalette = function(req, res) {
 	// get a random palette from the top ones
+	var paletteID = req.params.id;
 	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.send('Your random palette is called: ' + randomPalette['title']);
+	var palette = palettes[paletteID-1];
+	res.json(palette);
 }
